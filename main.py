d77afd57ab0333abd9e68b483afe93ce0b906ce5
hackgames13 <159486747+hackgames13@users.noreply.github.com>
2025-06-29 14:49:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 import asyncio
 import os
 from discord.ext import commands
+from keep_alive import keep_alive
+keep_alive()
 
 intents = discord.Intents.default()
 intents.message_content = True
